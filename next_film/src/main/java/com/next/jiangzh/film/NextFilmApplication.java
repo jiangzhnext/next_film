f9389279065c3b446977db5148005cd9905fd3ea
jiangzh <jiangzh@sina.com>
2019-02-26 00:20:07 +0800

    next_filmlm_init

---
+++ next_film/src/main/java/com/next/jiangzh/film/NextFilmApplication.java
@@ -0,0 +1,13 @@
+package com.next.jiangzh.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextFilmApplication.class, args);
+	}
+
+}
