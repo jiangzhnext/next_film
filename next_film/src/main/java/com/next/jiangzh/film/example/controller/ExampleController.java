7e371dc613eb37c85bcd7562b7e0a18ff8c5e4fe
jiangzh <jiangzh@sina.com>
2019-03-15 08:35:59 +0800

    Swagger2演示

--- next_film/src/main/java/com/next/jiangzh/film/example/controller/ExampleController.java
+++ next_film/src/main/java/com/next/jiangzh/film/example/controller/ExampleController.java
@@ -1,18 +1,26 @@
 package com.next.jiangzh.film.example.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/v1/example/")
+@Api("exampleController 相关的 api")
 public class ExampleController {
 
+    @ApiOperation(value = "测试SwaggerValue", notes = "测试SwaggerNotes")
+    @ApiImplicitParam(name = "test",
+            value = "入参str", paramType = "query", required = true, dataType = "string")
     @RequestMapping(value = "test")
-    public String test(String str){
+    public String test(String test){
 
-        System.out.println("str="+str);
+        System.out.println("str="+test);
 
-        return "test="+str;
+        return "test="+test;
     }
 
 }
