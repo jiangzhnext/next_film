ed9a1c69498f322251069860b4822415b29ba914
jiangzh <jiangzh@sina.com>
2019-04-27 20:38:03 +0800

    【数据层】数据层单元测试验证

---
+++ next_film/src/test/java/com/next/jiangzh/film/dao/mapper/FilmMapperTest.java
@@ -0,0 +1,52 @@
+package com.next.jiangzh.film.dao.mapper;
+
+import com.next.jiangzh.film.controller.cinema.vo.CinemaFilmInfoVO;
+import com.next.jiangzh.film.controller.cinema.vo.CinemaFilmVO;
+import com.next.jiangzh.film.controller.cinema.vo.FieldHallInfoVO;
+import com.next.jiangzh.film.controller.film.vo.ActorResultVO;
+import com.next.jiangzh.film.controller.film.vo.FilmDetailResultVO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilmMapperTest {
+
+    @Autowired
+    private FilmInfoTMapper filmInfoTMapper;
+
+    @Autowired
+    private FilmActorTMapper actorMapper;
+
+    @Test
+    public void describeFilmInfoById() {
+        String filmId = "2";
+        FilmDetailResultVO filmDetailResultVO =
+                filmInfoTMapper.describeFilmDetailByFilmId(filmId);
+        System.out.println("filmDetailResultVO by Id :" + filmDetailResultVO);
+    }
+
+    @Test
+    public void describeFilmInfoByName() {
+        String filmName = "药神";
+        FilmDetailResultVO filmDetailResultVO =
+                filmInfoTMapper.describeFilmDetailByFilmName(filmName);
+        System.out.println("filmDetailResultVO by Id :" + filmDetailResultVO);
+    }
+
+    @Test
+    public void describeActorsById() {
+        String filmId = "2";
+        List<ActorResultVO> actors = actorMapper.describeActorsByFilmId(filmId);
+        actors.stream().forEach(
+//                (actor) -> System.out.println(actor)
+                System.out::print
+        );
+    }
+
+}
