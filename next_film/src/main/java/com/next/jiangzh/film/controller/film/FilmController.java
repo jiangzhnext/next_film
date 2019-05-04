59f9128041c0a05206fc3a93f66eda7075f50282
jiangzh <jiangzh@sina.com>
2019-05-04 22:36:07 +0800

    【表现层】Film表现层结构搭建

--- next_film/src/main/java/com/next/jiangzh/film/controller/film/FilmController.java
+++ next_film/src/main/java/com/next/jiangzh/film/controller/film/FilmController.java
@@ -1,4 +1,61 @@
 package com.next.jiangzh.film.controller.film;
 
+import com.next.jiangzh.film.controller.common.BaseResponseVO;
+import com.next.jiangzh.film.controller.film.vo.request.DescribeFilmListReqVO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/film")
 public class FilmController {
+
+    /*
+        获取首页信息
+     */
+    @RequestMapping(value = "/getIndex",method = RequestMethod.GET)
+    public BaseResponseVO describeIndex(){
+
+        return null;
+    }
+
+
+    /*
+        获取查询条件列表
+     */
+    @RequestMapping(value = "/getConditionList",method = RequestMethod.GET)
+    public BaseResponseVO getConditionList(
+           @RequestParam(name = "catId",required = false,defaultValue = "99") String catId,
+           @RequestParam(name = "sourceId",required = false,defaultValue = "99") String sourceId,
+           @RequestParam(name = "yearId",required = false,defaultValue = "99") String yearId
+    ){
+
+
+
+        return null;
+    }
+
+
+    /*
+        获取电影列表信息
+     */
+    @RequestMapping(value = "/getFilms",method = RequestMethod.GET)
+    public BaseResponseVO getFilms(DescribeFilmListReqVO requestVO){
+
+        return null;
+    }
+
+
+    /*
+        获取电影列表信息
+     */
+    @RequestMapping(value = "/films/{searchStr}",method = RequestMethod.GET)
+    public BaseResponseVO describeFilmDetails(@PathVariable(name = "searchStr") String searchStr,String searchType){
+
+
+
+        return null;
+    }
+
+
+
+
 }
