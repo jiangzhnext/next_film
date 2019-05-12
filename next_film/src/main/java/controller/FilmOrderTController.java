67910c85f23d021d68b56a33b6bcc5e19b17a4db
jiangzh <jiangzh@sina.com>
2019-05-12 22:42:20 +0800

    订单模块数据初始化

---
+++ next_film/src/main/java/controller/FilmOrderTController.java
@@ -0,0 +1,20 @@
+package controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 订单信息表 前端控制器
+ * </p>
+ *
+ * @author jiangzh
+ * @since 2019-05-12
+ */
+@Controller
+@RequestMapping("/filmOrderT")
+public class FilmOrderTController {
+
+}
---
+++ next_film/src/main/java/service/impl/FilmOrderTServiceImpl.java
@@ -0,0 +1,20 @@
+package service.impl;
+
+import com.next.jiangzh.film.dao.entity.FilmOrderT;
+import com.next.jiangzh.film.dao.mapper.FilmOrderTMapper;
+import service.IFilmOrderTService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 订单信息表 服务实现类
+ * </p>
+ *
+ * @author jiangzh
+ * @since 2019-05-12
+ */
+@Service
+public class FilmOrderTServiceImpl extends ServiceImpl<FilmOrderTMapper, FilmOrderT> implements IFilmOrderTService {
+
+}
